f430eb18bb375587435b95a21813c361f2585e6a
Paulo Vitor Souza <pvitors@me.com>
2018-08-20 20:57:22 -0300

    First edits in Math obj
    Only supposed to fix imports from changed names in potigolutil

--- src/main/scala/br/edu/ifrn/potigol/Matematica.scala
+++ src/main/scala/br/edu/ifrn/potigol/Matematica.scala
@@ -31,7 +31,7 @@
 
 package br.edu.ifrn.potigol
 
-import Potigolutil.{ Colecao, Inteiro, Real }
+import Potigolutil.{ Colecao, Integer, Real }
 
 object Matematica {
   def sen(a: Real): Real = Math.sin(a)
@@ -41,20 +41,20 @@ object Matematica {
   def arccos(a: Real): Real = Math.acos(a)
   def arctg(a: Real): Real = Math.atan(a)
   def abs(a: Real): Real = Math.abs(a)
-  def abs(a: Inteiro): Inteiro = Math.abs(a)
+  def abs(a: Integer): Integer = Math.abs(a)
   def raiz(a: Real, b: Real = 2.0): Real = Math.pow(a, 1.0 / b)
   val PI: Real = Math.PI
   def log(a: Real): Real = Math.log(a)
   def log10(a: Real): Real = Math.log10(a)
   def aleatório(): Real = Math.random()
   def aleatorio: Real = aleatório
-  def aleatório(ultimo: Inteiro): Inteiro = aleatório(1, ultimo)
-  def aleatorio(primeiro: Inteiro): Inteiro = aleatório(primeiro)
-  def aleatório(primeiro: Inteiro, ultimo: Inteiro): Inteiro = {
+  def aleatório(ultimo: Integer): Integer = aleatório(1, ultimo)
+  def aleatorio(primeiro: Integer): Integer = aleatório(primeiro)
+  def aleatório(primeiro: Integer, ultimo: Integer): Integer = {
     val faixa = ultimo - primeiro + 1
     (Math.random() * faixa).toInt + primeiro
   }
-  def aleatorio(primeiro: Inteiro, ultimo: Inteiro): Inteiro = aleatório(primeiro, ultimo)
+  def aleatorio(primeiro: Integer, ultimo: Integer): Integer = aleatório(primeiro, ultimo)
   def aleatório[T](lista: Colecao[T]): T = lista.get(aleatorio(lista.tamanho))
   def aleatorio[T](lista: Colecao[T]): T = aleatório(lista)
 }
